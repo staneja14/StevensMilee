f2399710fc3675534cd459ef65f7cbfa4f40be11
Shivani Taneja <shivani@Shivanis-MBP.fios-router.home>
2020-11-25 14:45:09 -0500

    Added the query for fetching the comments

--- coursesreview/src/main/java/com/stevensportal/coursesreview/repository/CommentRepository.java
+++ coursesreview/src/main/java/com/stevensportal/coursesreview/repository/CommentRepository.java
@@ -1,6 +1,10 @@
 package com.stevensportal.coursesreview.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.stevensportal.coursesreview.entity.Comment;
@@ -11,5 +15,8 @@ import com.stevensportal.coursesreview.entity.Comment;
  */
 @Repository
 public interface CommentRepository extends CrudRepository<Comment, Integer> {
+	
+	@Query("select c from Comment c where c.courseId = :courseId")
+	List<Comment> findReviewsByCourseId(@Param("courseId") int courseId);
 
 }

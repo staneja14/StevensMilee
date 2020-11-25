e151b2f67e919ac9d5cc8868dde71439e6159a8a
Shivani Taneja <shivani@Shivanis-MBP.fios-router.home>
2020-11-25 14:47:18 -0500

    Added method to fetch reviews

--- coursesreview/src/main/java/com/stevensportal/coursesreview/service/CoursesReviewService.java
+++ coursesreview/src/main/java/com/stevensportal/coursesreview/service/CoursesReviewService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.stevensportal.coursesreview.contract.response.CoursesResponseDTO;
 import com.stevensportal.coursesreview.contract.response.LoginResponseDTO;
 import com.stevensportal.coursesreview.contract.response.ProfessorResponseDTO;
+import com.stevensportal.coursesreview.contract.response.ReviewsResponseDTO;
 import com.stevensportal.coursesreview.contract.response.StudentResponseDTO;
 import com.stevensportal.coursesreview.contract.response.base.ErrorDTO;
 
@@ -38,6 +39,31 @@ public interface CoursesReviewService {
 	 */
 	CoursesResponseDTO fetchCoursesInfo(Integer studentId, List<ErrorDTO> errorList);
 
+	/**
+	 * 
+	 * @param courseId
+	 * @param errorList
+	 * @return
+	 */
 	ProfessorResponseDTO fetchProfessorInfo(Integer courseId, List<ErrorDTO> errorList);
+	
+
+	/**
+	 * 
+	 * @param courseId
+	 * @param errorList
+	 * @return
+	 */
+	ReviewsResponseDTO fetchReviews(Integer courseId, List<ErrorDTO> errorList);
+
+	/**
+	 * 
+	 * @param studentId
+	 * @param courseId
+	 * @param description
+	 * @param errorList
+	 */
+	void postTheReview(int studentId, int courseId, String description, List<ErrorDTO> errorList);
+
 	
 }
